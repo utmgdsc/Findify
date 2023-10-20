4e1a058185c27f8e67f57b403308a5828fb655fb
Goutam112 <goutams112345@gmail.com>
2023-10-20 14:49:55 -0400

    fixed small error

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route('/testLogin')
   .get(authenticate, (req, res) => {
-    res.json({s message: `Welcome ${req.user.email}` });
+    res.json({ message: `Welcome ${req.user.email}` });
   });
 
 router.route('/register')
