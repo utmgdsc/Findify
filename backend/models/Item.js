b973a68ae3797905429468609cbdc514e1ff0d5e
Harshvardhan Gajabi <harsh.gajabi@mail.utoronto.ca>
2023-10-29 16:37:22 -0400

    Fix LostItem and FoundItem mongoDB schemas

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -1,44 +1,50 @@
 const mongoose = require('mongoose');
 
-const LostItemSchema = new mongoose.Schema({
-  dateCreated: { type: Date, default: Date.now() },
-  type: { type: String, required: true },
-  brand: { type: String, required: true },
-  // * added categories for size such as small, medium, large, x-large. We can remove if not needed
-  size: { type: String, enum: ['sm', 'md', 'lg', 'xl'], required: true },
-  colour: { type: String, required: true },
-  image: { data: Buffer, contentType: String },
-  locationLost: { type: String, required: true },
-  timeLost: { type: Date, default: Date.now() },
-  description: { type: String },
-  host: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true }, // could add default value of admin
-  isActive: { type: Boolean, default: true },
-  matchedLostItem: { type: mongoose.Schema.Types.ObjectId, ref: 'foundItem', default: null }
-});
+const LostItemSchema = new mongoose.Schema(
+  {
+    type: { type: String, required: true },
+    brand: { type: String, required: true },
+    size: { type: String, enum: ['S', 'M', 'L', 'XL'], required: true },
+    colour: { type: String, required: true },
+    locationLost: { type: String, required: true },
+    host: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true }, // could add default value of admin
 
-const FoundItemSchema = new mongoose.Schema({
-  dateCreated: { type: Date, default: Date.now() },
-  type: { type: String, required: true },
-  brand: { type: String, required: true },
-  // * added categories for size such as small, medium, large, x-large. We can remove if not needed 
-  size: { type: String, enum: ['sm', 'md', 'lg', 'xl'], required: true },
-  colour: { type: String, required: true },
-  image: { data: Buffer, contentType: String },
-  locationFound: { type: String, required: true },
-  timeFound: { type: Date, default: Date.now() },
-  description: { type: String },
-  host: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true }, // could add default value of admin
-  isActive: { type: Boolean, default: true },
-  matchedFoundItem: { type: mongoose.Schema.Types.ObjectId, ref: 'lostItem', default: null }
-})
+    // optional fields:
+    imageUrl: { type: String, default: '' },
+    timeLost: { type: Date, default: Date.now },
+    description: { type: String, default: '' },
+    isActive: { type: Boolean, default: true },
+    matchedFoundItem: { type: mongoose.Schema.Types.ObjectId, ref: 'foundItem', default: null }
+  },
+  { timestamps: true }
+);
+
+const FoundItemSchema = new mongoose.Schema(
+  {
+    type: { type: String, required: true },
+    brand: { type: String, required: true },
+    size: { type: String, enum: ['S', 'M', 'L', 'XL'], required: true },
+    colour: { type: String, required: true },
+    locationFound: { type: String, required: true },
+    host: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true }, // could add default value of admin
+
+    // optinal fields:
+    imageUrl: { type: String, default: '' },
+    timeFound: { type: Date, default: Date.now },
+    description: { type: String, default: '' },
+    isActive: { type: Boolean, default: true },
+    matchedLostItem: { type: mongoose.Schema.Types.ObjectId, ref: 'lostItem', default: null }
+  },
+  { timestamps: true }
+);
 
 const PotentialMatchSchema = new mongoose.Schema({
   lostId: { type: mongoose.Schema.Types.ObjectId, ref: 'lostItem', required: true },
   foundId: { type: mongoose.Schema.Types.ObjectId, ref: 'foundItem', required: true }
-})
+});
 
 const lostItemModel = mongoose.model('lostItem', LostItemSchema);
 const foundItemModel = mongoose.model('foundItem', FoundItemSchema);
-const potentialMatchModel = mongoose.model('potentialMatch', PotentialMatchSchema)
+const potentialMatchModel = mongoose.model('potentialMatch', PotentialMatchSchema);
 
 module.exports = { LostItem: lostItemModel, FoundItem: foundItemModel, PotentialMatch: potentialMatchModel };
