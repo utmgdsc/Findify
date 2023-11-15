b51b3c24ff107e2e987fac1437b7680e93d3ce65
Goutam112 <goutams112345@gmail.com>
2023-11-15 02:05:03 -0500

    fixed navbar styling

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,14 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+    exposedHeaders: ["Authorization"],
+  })
+);
+
 // Connect to MongoDB
 connectDB();
 
