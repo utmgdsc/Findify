9d6d1415572b1b01a23ff0a95ca0b0ce043b469d
Goutam112 <goutams112345@gmail.com>
2023-11-15 18:42:39 -0500

    added routes & controllers for admin getting all data

---
+++ backend/routes/admin.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { checkRequiredAttributes, adminAuthenticate } = require('../middlewares/user');
+const AdminController = require('../controllers/admin');
+const multerUpload = require("../middlewares/multer");
+
+const router = express.Router();
+
+router.route('/allLostItems')
+  .get(
+    adminAuthenticate,
+    async (req, res) => {
+      try {
+        await AdminController.getAllLostItems(req, res, errorHandler);
+      } catch (err) {
+        res.json({ message: `Get All Lost Items Error: ${err}` })
+      }
+    }
+  )
+router.route('/allFoundItems')
+  .get(
+    adminAuthenticate,
+    async (req, res) => {
+      try {
+        await AdminController.getAllFoundItems(req, res, errorHandler);
+      } catch (err) {
+        res.json({ message: `Get All Found Items Error: ${err}` })
+      }
+    }
+  )
+router.route('/allPotentialMatches')
+  .get(
+    adminAuthenticate,
+    async (req, res) => {
+      try {
+        await AdminController.getAllPotentialMatches(req, res, errorHandler);
+      } catch (err) {
+        res.json({ message: `Get All Potential Matches Error: ${err}` })
+      }
+    }
+  )
+
+
+const errorHandler = (err) => { }
+
+module.exports = router;
